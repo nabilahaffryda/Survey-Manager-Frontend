5caf014926d641e95fa33ce9e1f6496c81d1f056
nabilahaffryda <sehungantengs@gmail.com>
2021-08-05 13:48:06 +0700

    fix header for team

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,12 +10,19 @@
                 <v-btn text @click="redirectSurvey()">
                    Survey
                 </v-btn>
-                <v-btn text @click="redirectTeamMember()">
-                   Team Member
-                </v-btn>
-                <v-btn text @click="redirectTeamSurvey()">
-                   Team Survey
-                </v-btn>
+                <v-menu offset-y>
+                    <template v-slot:activator="{ on }">
+                        <v-btn v-on="on" text color="white">
+                            <span>Team</span>
+                            <v-icon right>mdi-chevron-down</v-icon>
+                        </v-btn>
+                    </template>
+                    <v-list>
+                        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                            <v-list-item-title>{{ link.text }}</v-list-item-title>
+                        </v-list-item>
+                    </v-list>
+                </v-menu>
                 <v-menu offset-y>
                     <template v-slot:activator="{ on }">
                         <v-btn v-on="on" text color="white">
@@ -24,7 +31,7 @@
                         </v-btn>
                     </template>
                     <v-list>
-                        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                        <v-list-item v-for="link in profile" :key="link.text" router :to="link.route">
                             <v-list-item-title>{{ link.text }}</v-list-item-title>
                         </v-list-item>
                     </v-list>
@@ -53,8 +60,12 @@ export default {
     data() {
         return {
             links: [
-                // {icon: 'survey', text: 'Survey', route: '/surveylist'},
+                {icon: 'team', text: 'Team Member', route: 'teammemberlist'},
+                {icon: 'team', text: 'Team Survey', route: 'teamsurveylist'},
             ],
+            profile: [
+                
+            ]
         }
     },
     computed: {
@@ -77,12 +88,6 @@ export default {
         redirectSurvey(){
             this.$router.push('/surveylist')
         },
-        redirectTeamMember(){
-            this.$router.push({name: 'team-member-list'})
-        },
-        redirectTeamSurvey(){
-            this.$router.push({name: 'team-survey-list'})
-        }
     },
 }
 </script>
